1cd23615b9b3885217c42f1395500333b6e4b8d0
Dwayne Anderson <dwayne.anderson@gmail.com>
2014-11-13 16:41:21 -0800

    add ohmyzsh + custom theme

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-SH_FILES=".gitconfig"
+SH_FILES=".gitconfig .zshrc"
 BACKUP="backups/`date +'%Y%m%d-%H%M%S'`"
 SHDIR=$PWD
 
+# OH MY ZSH
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+# CREATE DOT FILES FROM LIST ABOVE
 for FILE in $SH_FILES; do
     if [ -e "$HOME/$FILE" -o -L "$HOME/$FILE" ]; then
         if [ -L "$HOME/$FILE" ]; then
